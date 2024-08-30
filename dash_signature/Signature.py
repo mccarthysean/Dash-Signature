11bf0c09ffb297df9948911ae708c8d2b036863c
Coulton.Fraser <coulton.fraser@ovintiv.com>
2024-08-29 21:33:22 -0600

    chore: rebuild the dash component

--- dash_signature/Signature.py
+++ dash_signature/Signature.py
@@ -12,12 +12,6 @@ Keyword arguments:
 - id (string; optional):
     The ID used to identify this component in Dash callbacks.
 
-- resize (boolean; default False)
-
-- save (boolean; default False):
-    The trigger for the component to update the value.  Setting to
-    True will update the value.
-
 - value (string; default ''):
     The value displayed in the input."""
     _children_props = []
@@ -25,10 +19,10 @@ Keyword arguments:
     _namespace = 'dash_signature'
     _type = 'Signature'
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, value=Component.UNDEFINED, save=Component.UNDEFINED, resize=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'resize', 'save', 'value']
+    def __init__(self, id=Component.UNDEFINED, value=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'value']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'resize', 'save', 'value']
+        self.available_properties = ['id', 'value']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
