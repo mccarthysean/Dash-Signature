7006cc6c752e011665fa64700f59718fc5cbdfff
Sean McCarthy <sean.mccarthy@hotmail.com>
2024-10-15 11:14:35 -0600

    0.1.5 used updated React stuff from Coulton

--- src/DashSignature.jl
+++ src/DashSignature.jl
@@ -3,7 +3,7 @@ module DashSignature
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.1.4"
+const version = "0.1.5"
 
 include("jl/signature.jl")
 
